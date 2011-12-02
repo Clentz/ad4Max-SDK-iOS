bfb58036a1d0aaaad48b3e0a5c92c0cc8669c87f
Cyril <cyril.picat@gmail.com>
2011-12-02 12:40:21 +0100

    Add unit test framework GHUnit

---
+++ ad4Max-Tests/Ad4MaxParamsServiceTests.m
@@ -0,0 +1,43 @@
+//
+//
+//  Ad4MaxParamsServiceTests.m
+//  ad4Max-SampleApp
+//
+//  Copyright 2011 Publigroupe
+//
+//  Licensed under the Apache License, Version 2.0 (the "License"); you may not
+//  use this file except in compliance with the License.  You may obtain a copy
+//  of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+//  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+//  License for the specific language governing permissions and limitations under
+//  the License.
+//
+
+// ============================================================================
+
+
+#import <GHUnitIOS/GHUnit.h> 
+
+@interface Ad4MaxParamsServiceTests : GHTestCase { }
+@end
+
+@implementation Ad4MaxParamsServiceTests
+
+- (void)testStrings {       
+    NSString *string1 = @"a string";
+    GHTestLog(@"I can log to the GHUnit test console: %@", string1);
+    
+    // Assert string1 is not NULL, with no custom error description
+    GHAssertNotNULL(string1, nil);
+    
+    // Assert equal objects, add custom error description
+    NSString *string2 = @"a string 2";
+    GHAssertEqualObjects(string1, string2, @"A custom error message. string1 should be equal to: %@.", string2);
+}
+
+@end
---
+++ ad4Max-Tests/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ad4Max-Tests
+//
+//  Created by Cyril Picat on 12/2/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[])
+{
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"GHUnitIPhoneAppDelegate");
+    [pool release];
+    return retVal;
+}
